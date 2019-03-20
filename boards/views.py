e207455da8946e6c0784dd057655924ef7db3ce4
mukolweke <michaelolukak@gmail.com>
2019-03-20 08:22:37 +0300

    templates, static files and test

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from .models import Board
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+
+    return render(request, 'home.html', {'boards' : boards})
